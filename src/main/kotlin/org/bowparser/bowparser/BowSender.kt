188246816eb5426dd37296c0364f6e9d2a3af39b
void-spark <81029971+void-spark@users.noreply.github.com>
2023-06-08 00:15:38 +0200

    Some refactoring, missed a class

---
+++ src/main/kotlin/org/bowparser/bowparser/BowSender.kt
@@ -0,0 +1,12 @@
+package org.bowparser.bowparser
+
+import com.fazecast.jSerialComm.SerialPort
+
+fun send(cmd: List<UByte>, comPort: SerialPort) {
+    val outList = cmd.toMutableList()
+    outList.add(0, 0x10u)
+    outList.add(CRC8().crc8Bow(outList))
+
+    val escaped = outList.flatMapIndexed { ind, it -> if (ind != 0 && it.toUInt() == 0x10u) listOf(it, it) else listOf(it) }
+    comPort.writeBytes(escaped.toUByteArray().toByteArray(), escaped.size.toLong())
+}
