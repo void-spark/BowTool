7f68f789002a0a3b963e6bcf0eb85567dede12a1
void-spark <81029971+void-spark@users.noreply.github.com>
2023-06-13 21:05:08 +0200

    Fully read array types

--- src/main/kotlin/org/bowparser/bowparser/Scan.kt
+++ src/main/kotlin/org/bowparser/bowparser/Scan.kt
@@ -29,7 +29,7 @@ class Scanner(private val serialPort: SerialPort, private val baudRate: Int, pri
     private val results = ArrayList<Pair<Message, Message>>()
     private val decoder = GetDataDecoder(dataIdsByInt)
     private val parser = MessageParser(this::handleMessage) { message -> println("Incomplete: ${hex(message)}, crc:${hex(CRC8().crc8Bow(message.dropLast(1)))}") }
-
+    private var arrOffset = 0u
     private var waited = 0
 
     fun scan() {
@@ -40,7 +40,11 @@ class Scanner(private val serialPort: SerialPort, private val baudRate: Int, pri
         while (state != State.DONE) {
             if (state == State.SEND_COMMAND) {
                 request = null
-                sendGetData(serialPort, target, types[typePos], toScan[idPos])
+                if (TypeFlags(types[typePos]).array) {
+                    sendGetDataArray(serialPort, target, types[typePos], toScan[idPos], arrOffset.toUByte())
+                } else {
+                    sendGetData(serialPort, target, types[typePos], toScan[idPos])
+                }
                 state = State.WAIT_RESPONSE
                 continue
             }
@@ -108,8 +112,28 @@ class Scanner(private val serialPort: SerialPort, private val baudRate: Int, pri
                     // Requested type does not match stored type
                     idPos++
                 } else {
-                    results.add(Pair(request!!, message))
-                    toScan.removeAt(idPos)
+                    var storeResult = true
+                    var toNext = true
+
+                    val type = request!!.data()[0]
+                    if(TypeFlags(type).array) {
+                        val respArrLength = message.data()[3]
+                        if(respArrLength.toUInt() != 0u) {
+                            // More to read,
+                            arrOffset += respArrLength
+                            toNext = false
+                        } else {
+                            // Nothing was left to read, don't store result
+                            arrOffset = 0u
+                            storeResult = false
+                        }
+                    }
+                    if(storeResult) {
+                        results.add(Pair(request!!, message))
+                    }
+                    if(toNext) {
+                        toScan.removeAt(idPos)
+                    }
                 }
                 if (idPos == toScan.size) {
                     if (toScan.size == 0 || typePos == types.size - 1) {
@@ -128,21 +152,22 @@ class Scanner(private val serialPort: SerialPort, private val baudRate: Int, pri
     }
 }
 
-private fun sendGetData(serialPort: SerialPort, target: UByte, type: UByte, id: UByte, offset: UByte = 0x00u) {
-    val first = (target.toInt().shl(4) or 0x01).toUByte()
-    if (TypeFlags(type).array) {
-        send(listOf(first, 0x43u, 0x08u, type, id, offset), serialPort)
-    } else {
-        send(listOf(first, 0x42u, 0x08u, type, id), serialPort)
-    }
+private fun sendGetData(serialPort: SerialPort, target: UByte, type: UByte, id: UByte) {
+    send(listOf(byte1(target, 0x01u), 0x42u, 0x08u, type, id), serialPort)
+}
+
+private fun sendGetDataArray(serialPort: SerialPort, target: UByte, type: UByte, id: UByte, offset: UByte) {
+    send(listOf(byte1(target, 0x01u), 0x43u, 0x08u, type, id, offset), serialPort)
 }
 
 private fun sendPong(serialPort: SerialPort, target: UByte) {
-    val first = (target.toInt().shl(4) or 0x03).toUByte()
-    send(listOf(first, 0x40u), serialPort)
+    send(listOf(byte1(target, 0x03u), 0x40u), serialPort)
 }
 
 private fun sendWakeup(serialPort: SerialPort, target: UByte) {
-    val first = (target.toInt().shl(4) or 0x01).toUByte()
-    send(listOf(first, 0x40u, 0x14u), serialPort)
+    send(listOf(byte1(target, 0x01u), 0x40u, 0x14u), serialPort)
+}
+
+private fun byte1(target: UByte, type: UByte ) : UByte {
+    return (target.toUInt().shl(4) or type.toUInt()).toUByte()
 }
